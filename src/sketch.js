abcbb2b6415680104e3d2927280738e15be1d75d
Ben Lindow <benlinodw@gmail.com>
2022-07-23 21:57:50 -0500

    init

---
+++ src/sketch.js
@@ -0,0 +1,11 @@
+let winX = window.innerWidth;
+let winY = window.innerHeight;
+
+function setup() {
+  createCanvas(winX, winY);
+  background(128);
+}
+
+function draw() {
+  // put drawing code here
+}
